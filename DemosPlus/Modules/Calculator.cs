b9938399d5a50a30f691a796c2f32d45006f0030
goricher <goricherririri@gmail.com>
2023-11-21 01:33:21 +0800

    * excel

---
+++ DemosPlus/Modules/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemosPlus.Modules
+{
+    public class Calculator : IModule
+    {
+        public void OnResolve()
+        {
+        }
+
+        public void OnDestroy()
+        {
+        }
+
+        public float CalCost()
+        {
+            return 0f;
+        }
+
+    }
+
+    public class QCalculator : Query<Calculator> { }
+}
